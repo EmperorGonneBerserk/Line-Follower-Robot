67013d40eb367ee42e278be33b57ace1065b37da
shushruthgowda <132249899+EmperorGonneBerserk@users.noreply.github.com>
2025-11-10 16:53:16 +0530

    Implement command pattern for movement control
    Refactor line follower robot code to use command pattern for movement control.

--- line_follower.ino
+++ line_follower.ino
@@ -1,15 +1,7 @@
-// ------------------------------------------------------
-// Line Follower Robot using Arduino + L298N + 3 IR Sensors
-// Author: Shushrutha N Gowda
-// Description: A simple robot that follows a black line using
-// three IR sensors and controls movement via L298N motor driver.
-// ------------------------------------------------------
-
-#define LEFT_SENSOR   A0
+#define LEFT_SENSOR A0
 #define CENTER_SENSOR A1
-#define RIGHT_SENSOR  A2
+#define RIGHT_SENSOR A2
 
-// Motor driver pins
 #define ENA 5
 #define IN1 8
 #define IN2 9
@@ -17,8 +9,28 @@
 #define IN3 10
 #define IN4 7
 
-// Threshold for sensor detection
-#define THRESHOLD 500  // Adjust based on calibration (0–1023)
+#define THRESHOLD 500
+
+void moveForward();
+void turnLeft();
+void turnRight();
+void stopMotors();
+
+class DriveCmd {
+  public:
+    virtual void apply() = 0;
+    virtual ~DriveCmd() {}
+};
+
+class ForwardCmd : public DriveCmd { void apply() override { moveForward(); } };
+class LeftCmd    : public DriveCmd { void apply() override { turnLeft(); }    };
+class RightCmd   : public DriveCmd { void apply() override { turnRight(); }   };
+class StopCmd    : public DriveCmd { void apply() override { stopMotors(); }  };
+
+ForwardCmd FORWARD_CMD;
+LeftCmd    LEFT_CMD;
+RightCmd   RIGHT_CMD;
+StopCmd    STOP_CMD;
 
 void setup() {
   pinMode(LEFT_SENSOR, INPUT);
@@ -37,76 +49,59 @@ void setup() {
 }
 
 void loop() {
-  int leftValue = analogRead(LEFT_SENSOR);
+  int leftValue   = analogRead(LEFT_SENSOR);
   int centerValue = analogRead(CENTER_SENSOR);
-  int rightValue = analogRead(RIGHT_SENSOR);
+  int rightValue  = analogRead(RIGHT_SENSOR);
 
   Serial.print("L: "); Serial.print(leftValue);
-  Serial.print("  C: "); Serial.print(centerValue);
-  Serial.print("  R: "); Serial.println(rightValue);
+  Serial.print(" C: "); Serial.print(centerValue);
+  Serial.print(" R: "); Serial.println(rightValue);
 
-  bool leftDetect = leftValue < THRESHOLD;
+  bool leftDetect   = leftValue   < THRESHOLD;
   bool centerDetect = centerValue < THRESHOLD;
-  bool rightDetect = rightValue < THRESHOLD;
+  bool rightDetect  = rightValue  < THRESHOLD;
 
-  // Basic logic for line following:
+  DriveCmd* cmd = &FORWARD_CMD;
   if (centerDetect && !leftDetect && !rightDetect) {
-    moveForward();         // Line is centered
-  } 
-  else if (leftDetect && !centerDetect) {
-    turnLeft();            // Line is on the left
-  } 
-  else if (rightDetect && !centerDetect) {
-    turnRight();           // Line is on the right
-  } 
-  else if (!leftDetect && !centerDetect && !rightDetect) {
-    stopMotors();          // Line lost
-  } 
-  else {
-    moveForward();         // Default
+    cmd = &FORWARD_CMD;
+  } else if (leftDetect && !centerDetect) {
+    cmd = &LEFT_CMD;
+  } else if (rightDetect && !centerDetect) {
+    cmd = &RIGHT_CMD;
+  } else if (!leftDetect && !centerDetect && !rightDetect) {
+    cmd = &STOP_CMD;
+  } else {
+    cmd = &FORWARD_CMD;
   }
 
+  cmd->apply();
   delay(50);
 }
 
-// ------------------- Movement Functions -------------------
-
 void moveForward() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, 200);
-  analogWrite(ENB, 200);
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  analogWrite(ENA, 200); analogWrite(ENB, 200);
   Serial.println("Forward");
 }
 
 void turnLeft() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, 150);
-  analogWrite(ENB, 150);
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  analogWrite(ENA, 150); analogWrite(ENB, 150);
   Serial.println("Turn Left");
 }
 
 void turnRight() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  analogWrite(ENA, 150);
-  analogWrite(ENB, 150);
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
+  analogWrite(ENA, 150); analogWrite(ENB, 150);
   Serial.println("Turn Right");
 }
 
 void stopMotors() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
+  digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0); analogWrite(ENB, 0);
   Serial.println("Stop");
 }
